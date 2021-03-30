875a8b679bac3191686bc5bf823a0d16a1a71223
Kamal <31344909+kmldpsng@users.noreply.github.com>
2021-03-30 16:04:56 +0530

    Add project files.

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuestAPI.Model;
+
+namespace QuestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        static List<Quotes> _quotes = new List<Quotes>
+        {
+            new Quotes{ID = 1, Author = "kamal", Description = "The Dummy Project", Title = "The Dummy Project"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Quotes> Get()
+        {
+            return _quotes;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Quotes quotes)
+        {
+            _quotes.Add(quotes);
+        }
+    }
+}
