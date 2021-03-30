551c7da9ecf380d83b2dbb9bc92a1544f3b01c46
Kamal <31344909+kmldpsng@users.noreply.github.com>
2021-03-30 17:56:41 +0530

    added migrations and data annotations

--- Model/Quotes.cs
+++ Model/Quotes.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuestAPI.Model
 {
     public class Quotes
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
         public string Author { get; set; }
+        [Required]
         public string Description { get; set; }
+        public string Type { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
